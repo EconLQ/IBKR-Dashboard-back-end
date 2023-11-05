14aed644e8aedec71a1620bfc177c05ff8054923
EconLQ <zavliybogdan@gmail.com>
2023-11-05 19:11:42 +0200

    Fixed injection issue of PositionDao bean + make OrderDataRetrieval class to run servlet init.

--- src/main/java/com/liquidus/ibkrdasboardjee8/servlet/AppServlet.java
+++ src/main/java/com/liquidus/ibkrdasboardjee8/servlet/AppServlet.java
@@ -1,48 +1,47 @@
 package com.liquidus.ibkrdasboardjee8.servlet;
 
-import com.liquidus.ibkrdasboardjee8.dao.PositionDao;
 import com.liquidus.ibkrdasboardjee8.dao.PositionLocal;
+import com.liquidus.ibkrdasboardjee8.entity.Position;
 import com.liquidus.ibkrdasboardjee8.tws.OrderDataRetrieval;
 
-import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 @WebServlet(name = "AppServlet", urlPatterns = "/app-servlet")
 public class AppServlet extends HttpServlet {
 
-    PositionLocal positionBean = CDI.current().select(PositionDao.class).get();
+    @Inject
+    PositionLocal positionBean;
+    @Inject
+    OrderDataRetrieval app;
     private Logger logger = Logger.getLogger(AppServlet.class.getName());
 
     @Override
     public void init() throws ServletException {
         logger.info("Starting AppServlet...");
+        try {
+            app.run();
+        } catch (InterruptedException e) {
+            logger.warning("Failed to start application: " + e.getMessage());
+        }
         super.init();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        OrderDataRetrieval app = new OrderDataRetrieval();
         try {
-            resp.getWriter().println("Connected to an IB API successfully.\n" +
-                    "Data fetched accordingly...\n");
-
-            positionBean.getAllPositions().forEach(position -> {
-                try {
-                    // print each position
-                    resp.getWriter().println(position + "\n");
-                } catch (IOException e) {
-                    logger.warning("IOException: " + e.getMessage());
-                }
-            });
-            app.run();
-        } catch (InterruptedException e) {
-            logger.warning("InterruptedException: " + e.getMessage());
+            List<Position> positions = positionBean.getAllPositions();
+            req.setAttribute("positions", positions);
+            getServletContext().getRequestDispatcher("/positions.jsp").forward(req, resp);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
         }
     }
 }
