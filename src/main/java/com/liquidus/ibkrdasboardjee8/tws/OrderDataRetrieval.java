99603a7bbcd941988362bf1dec4627180e265789
EconLQ <zavliybogdan@gmail.com>
2023-11-08 00:07:03 +0200

    Separated connection logic from an app logic. Added new methods to get portfolio updates and PnL that can be later used in AppServlet

--- src/main/java/com/liquidus/ibkrdasboardjee8/tws/OrderDataRetrieval.java
+++ src/main/java/com/liquidus/ibkrdasboardjee8/tws/OrderDataRetrieval.java
@@ -1,81 +1,67 @@
 package com.liquidus.ibkrdasboardjee8.tws;
 
 import com.ib.client.EClientSocket;
-import com.ib.client.EReader;
 import com.ib.client.EReaderSignal;
 import com.liquidus.ibkrdasboardjee8.dao.PositionLocal;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.IOException;
+import java.io.Serializable;
 import java.util.logging.Logger;
 
 @Named
-@ApplicationScoped
-public class OrderDataRetrieval {
+@SessionScoped
+public class OrderDataRetrieval implements Serializable {
     private static final String ACCOUNT_CODE = "DU6742034";
     private static Logger logger = Logger.getLogger(OrderDataRetrieval.class.getName());
-
+    private final EWrapperImpl wrapper = new EWrapperImpl();
+    private final EClientSocket clientSocket = wrapper.getClient();
+    private final EReaderSignal readerSignal = wrapper.getSignal();
     @Inject
     PositionLocal positionBean;
+    @Inject
+    TWSConnection twsConnection;
 
     public OrderDataRetrieval() {
     }
 
-    private static void pnl(EClientSocket client) throws InterruptedException {
-        client.reqPnL(1, ACCOUNT_CODE, "");
-
-        Thread.sleep(5000);
-
-        client.cancelPnL(1);
+    public void run() throws InterruptedException {
+        twsConnection.run(clientSocket, readerSignal);
     }
 
-    public void run() throws InterruptedException {
-        logger.info("Connecting to IB API socket...");
-        EWrapperImpl wrapper = new EWrapperImpl();
-        final EClientSocket clientSocket = wrapper.getClient();
-        final EReaderSignal readerSignal = wrapper.getSignal();
-
-
-//        clientSocket.eConnect("127.0.0.1", 7496, 3);  // TWS connection
-        clientSocket.eConnect("127.0.0.1", 4002, 3);    // IB Gateway connection
-
-        final EReader reader = new EReader(clientSocket, readerSignal);
-
-        reader.start();
-        new Thread(() -> {
-            while (clientSocket.isConnected()) {
-                readerSignal.waitForSignal();
-                try {
-                    reader.processMsgs();
-                } catch (IOException e) {
-                    logger.warning("Failed processing messages from EReader: " + e.getMessage());
-                }
-            }
-        }).start();
-
-        Thread.sleep(1000);
-
+    public void getPortfolioUpdates() {
         clientSocket.reqPositions();
-        Thread.sleep(3000); // sleep to ensure all the positions are added to the list (wrapper.getPositions())
+        try {
+            Thread.sleep(1000);
+            // request account updates (from TWS -> Account -> Account Window)
+            clientSocket.reqAccountUpdates(true, ACCOUNT_CODE);
+            Thread.sleep(1000);
 
-        // request account updates (from TWS -> Account -> Account Window)
-        clientSocket.reqAccountUpdates(true, ACCOUNT_CODE);
-        Thread.sleep(3000);
+            // add positions to Position table
+            logger.info("Get a list of all positions from the table Position: ");
+            wrapper.getPositions().forEach(position -> positionBean.addPosition(position));
+        } catch (InterruptedException e) {
+            logger.warning("Connection was interrupted: " + e.getMessage());
+        }
+    }
 
-        // get portfolio's Realized PnL, Unrealized PnL and Daily PnL
-        pnl(clientSocket);
-        Thread.sleep(3000);
 
-        // add positions to Position table
-        wrapper.getPositions().forEach(position -> positionBean.addPosition(position));
+    /**
+     * Get Unrealized PnL from {@link EWrapperImpl#updateAccountValue(String, String, String, String)}
+     *
+     * @return portfolio unrealized PnL from the account updates
+     */
+    public double portfolioUnrealizedPnL() {
+        return wrapper.getPortfolioUnrealizedPnL();
+    }
 
-        logger.info("Get a list of all positions from the table Position: ");
-        positionBean.getAllPositions().forEach(System.out::println);
-
-//        wrapper.getPositionsSet().forEach(position -> logger.info("Positions Set: " + position));
-        Thread.sleep(1000);  // sleep for 1s before disconnecting
-        clientSocket.eDisconnect();
+    /**
+     * Get Realized PnL from {@link EWrapperImpl#updateAccountValue(String, String, String, String)}
+     *
+     * @return portfolio realized PnL from the account updates
+     */
+    public double portfolioRealizedPnL() {
+        return wrapper.getPortfolioRealizedPnL();
     }
 }
