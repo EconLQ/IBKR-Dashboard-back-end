d1c16ad2bf76b7679f992f258dfa43d59ad0e479
EconLQ <zavliybogdan@gmail.com>
2024-01-03 00:18:26 +0200

    Updated AppServlet to return JSON object on response

--- src/main/java/com/liquidus/ibkrdasboardjee8/servlet/AppServlet.java
+++ src/main/java/com/liquidus/ibkrdasboardjee8/servlet/AppServlet.java
@@ -1,17 +1,18 @@
 package com.liquidus.ibkrdasboardjee8.servlet;
 
 import com.liquidus.ibkrdasboardjee8.dao.PositionLocal;
-import com.liquidus.ibkrdasboardjee8.entity.Position;
 import com.liquidus.ibkrdasboardjee8.tws.OrderDataRetrieval;
 
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.io.PrintWriter;
 import java.util.logging.Logger;
 
 @WebServlet(name = "AppServlet", urlPatterns = "/app-servlet")
@@ -25,43 +26,45 @@ public class AppServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
+        super.init();
         logger.info("Starting AppServlet...");
         // run application - init EWrapperImpl, get EClient and EReader signal
         app.run();
-        super.init();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            if (app.isConnected()) {
-                // request portfolio updates from the app
-                app.getPortfolioUpdates();
-                // calculate portfolio PnL
-                double portfolioUnrealizedPnL = app.portfolioUnrealizedPnL();
-                double portfolioRealizedPnL = app.portfolioRealizedPnL();
-                double portfolioNetLiquidation = app.portfolioNetLiquidation();
+        PrintWriter out = resp.getWriter();
 
-                List<Position> positions = positionBean.getAllPositions();
-                req.setAttribute("positions", positions);
-                req.setAttribute("portfolioUnrealizedPnL", portfolioUnrealizedPnL);
-                req.setAttribute("portfolioRealizedPnL", portfolioRealizedPnL);
-                req.setAttribute("portfolioNetLiquidation", portfolioNetLiquidation);
+        resp.setContentType("application/json");
+        // Create a JSON object to return
+        JsonObject json = null;
+        if (app.isConnected()) {
+            // request portfolio updates from the app
+            app.getPortfolioUpdates();
+            // build response JSON object
+            json = Json.createObjectBuilder()
+                    .add("status", "success")
+                    .build();
 
-                resp.setHeader("Refresh", "30; URL=app-servlet"); // refresh page each 30s
+            // send 200 on response
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            // app was disconnected as user logged out, try to connect one more time...
+            logger.warning("App is not connected to TWS/IB Gateway. Trying to establish connection...");
 
-                getServletContext().getRequestDispatcher("/positions.jsp").forward(req, resp);
-                // send 200 on response
-                resp.setStatus(HttpServletResponse.SC_OK);
-            } else {
-                // app was disconnected as user logged out, try to connect one more time...
-                logger.warning("App is not connected to TWS/IB Gateway. Trying to establish connection...");
-                app.run();
-                // send 202 to client
-                resp.setStatus(HttpServletResponse.SC_ACCEPTED);
-            }
-        } catch (ServletException e) {
-            throw new RuntimeException(e);
+            json = Json.createObjectBuilder()
+                    .add("status", "accepted")
+                    .build();
+            // send 202 to client
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+            // rerun the application to run update after connection
+            app.run();
+        }
+        if (json != null) {
+            out.print(json);
+            out.flush();
         }
     }
 }
